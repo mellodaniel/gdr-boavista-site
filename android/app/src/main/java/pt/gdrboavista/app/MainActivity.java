cc0e4085edb96fc353b421357e0394ad78f35574
Daniel Mello <danielmello@unknown7a99fd2f272a.lan>
2026-06-25 16:39:09 +0100

    add Android app APK download

---
+++ android/app/src/main/java/pt/gdrboavista/app/MainActivity.java
@@ -0,0 +1,5 @@
+package pt.gdrboavista.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'pt.gdrboavista.app',
+  appName: 'GDR Boavista',
+  webDir: 'dist'
+};
+
+export default config;
